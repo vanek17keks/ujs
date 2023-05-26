0c810ccac18f98c2f8330ba29bdb119ee7637181
ivan <ivan170801@gmail.com>
2023-05-27 02:16:26 +0300

    first commit

---
+++ u11js/js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(-4/0);
+
+console.log('string' * 9);
+
+const persone = "5";
+
+const bool = false;
+
+console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 23,
+    isMarried: false
+};
+
+console.log(obj.isMarried)
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[1]);
+
+
---
+++ u13js/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);
